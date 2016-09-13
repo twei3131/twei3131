abc174b0a713c62b2df4191673ef130a0b699507
twei3131 <848270978@qq.com>
2016-09-13 20:44:01 +0800

    毕业实训

--- WebRoot/js/teacher.js
+++ WebRoot/js/teacher.js
@@ -9,4 +9,15 @@ window.onload = function(){
 			$(this).css({"background-image":"url(./../img/register"+i+".jpg)"});
 		})
 	},30000);
+	aja();
+}
+
+function aja(){
+	if(window.localStorage['username'] != null && window.localStorage['password'] != null){
+		$.post("",{"username":localStorage['username'],"password":localStorage['password']},function(data){
+			
+		});
+	}else{
+		$.get("");
+	}
 }
