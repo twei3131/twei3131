514b9cc8fe7ada494ee42674136a118ac23a8838
twei3131 <848270978@qq.com>
2016-09-16 12:26:05 +0800

    毕业实训

--- src/com/twei3131/Controller/TestController.java
+++ src/com/twei3131/Controller/TestController.java
@@ -6,8 +6,11 @@ import java.util.List;
 import com.jfinal.core.Controller;
 import com.jfinal.plugin.activerecord.Record;
 import com.twei3131.common.model.Student;
+import com.twei3131.common.model.Subjectinfo;
 import com.twei3131.common.model.Teacher;
+import com.twei3131.common.model.Tempsign;
 import com.twei3131.service.Scan;
+import com.twei3131.service.Students;
 import com.twei3131.service.Teacheres;
 
 public class TestController extends Controller {
@@ -24,9 +27,16 @@ public class TestController extends Controller {
 		//System.out.println(scan.isExists("tea0001","11", "sub0001"));
 		//System.out.println(scan.isReScan("tea0001"));
 		//List<Record> teacheres = scan.getStudentInfoBySubjectId("tea0002", "sub0003", "gp0002");
-		List<Student> list = scan.getStudentInfoBySubjectId("tea0002", "sub0003", "gp0002");
-		scan.setStudentInfo(list, "tea0002", "sub0003");
-		System.out.println(list.get(0).getStr("studentName"));
+//		List<Student> list = scan.getStudentInfoBySubjectId("tea0002", "sub0003", "gp0002");
+//		scan.setStudentInfo(list, "tea0002", "sub0003");
+//		System.out.println(list.get(0).getStr("studentName"));
+//		String stuId = getSessionAttr("username").toString();
+//		String teaId = getSessionAttr("teaId").toString();
+//		String subId = getSessionAttr("subId");
+//		Subjectinfo subjectinfo = Subjectinfo.dao.findFirst("select * from subjectInfo where teacherId = '" + teaId + "' and subjectId='"+subId + "'");
+//		Tempsign tempsign = Tempsign.dao.findById(stuId, teaId);
+//		Students students = new Students();
+//		System.out.println(students.getStuStateByClassState("stuFirst", subjectinfo.getState(), stuId, teaId));
 		
 		render("/test/index.jsp");
 	}
