db7e531e36bf5589403b02f9ab3bd611ab605a93
twei3131 <848270978@qq.com>
2016-09-18 20:53:53 +0800

    毕业实训

--- src/com/twei3131/Controller/BacgrouController.java
+++ src/com/twei3131/Controller/BacgrouController.java
@@ -1,9 +1,28 @@
 package com.twei3131.Controller;
 
+import java.util.List;
+
 import com.jfinal.core.Controller;
+import com.twei3131.common.model.Signerror;
 
 public class BacgrouController extends Controller {
 	public void index(){
 		render("/bacgrou/main.jsp");
+	}
+	
+	public void audit(){
+		String instructorId = getPara("instructorId");
+		String subjectId = getPara("subjectId");
+		String classId = getPara("classId");
+		
+		String sql = "SELECT a.* FROM signerror a JOIN student b ON a.studentId = b.studentId "+
+		" JOIN classes c ON b.classId = c.classId JOIN instructor d ON c.instructorId = d.instructorId "+ 
+		"WHERE d.instructorId='"+instructorId+"' AND c.classId = '"+classId+"' AND a.subjectId = '"+subjectId+"'";
+		
+		List<Signerror> list = Signerror.dao.find(sql);
+		
+		setAttr("context", list);
+		
+		render("/bacgrou/table.jsp");
 	}
 }
