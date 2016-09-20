fe6f391b2179043a3239c2f597e019a7031ef716
twei3131 <848270978@qq.com>
2016-09-20 08:32:45 +0800

    毕业实训

---
+++ src/com/twei3131/service/ResolveExcel.java
@@ -0,0 +1,12 @@
+package com.twei3131.service;
+
+import java.io.File;
+
+import com.jfinal.kit.PathKit;
+
+public class ResolveExcel {
+	public final String basePath = PathKit.getWebRootPath()+File.separator+"twei3131Load"+File.separator;
+	public void studentRes(){
+		
+	}
+}
