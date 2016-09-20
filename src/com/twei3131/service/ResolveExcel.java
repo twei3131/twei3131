182dc2c304a96ee2ca512cf05b32785f1a979e7b
twei3131 <848270978@qq.com>
2016-09-20 14:27:06 +0800

    毕业实训

--- src/com/twei3131/service/ResolveExcel.java
+++ src/com/twei3131/service/ResolveExcel.java
@@ -4,6 +4,8 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.poi.hssf.usermodel.HSSFCell;
 import org.apache.poi.hssf.usermodel.HSSFRow;
@@ -11,12 +13,15 @@ import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 
 import com.jfinal.kit.PathKit;
+import com.jfinal.plugin.activerecord.Db;
+import com.twei3131.common.model.Student;
 
 public class ResolveExcel {
 	public final String basePath = PathKit.getWebRootPath()+File.separator+"twei3131Load"+File.separator;
 	@SuppressWarnings("resource")
-	public void studentRes(String filename) throws IOException{
+	public List<List<String>> resExcel(String filename) throws IOException{
 		InputStream inStream = new FileInputStream(basePath+filename+".xlsx");
+		List<List<String>> restult = new ArrayList<List<String>>();
 		HSSFWorkbook hssfWorkbook = new HSSFWorkbook(inStream);
 		for(int numSheet = 0;numSheet < hssfWorkbook.getNumberOfSheets();numSheet++){
 			HSSFSheet hssfSheet = hssfWorkbook.getSheetAt(numSheet);
@@ -28,14 +33,35 @@ public class ResolveExcel {
 				
 				int minCilIx = hssfRow.getFirstCellNum();
 				int maxCilIx = hssfRow.getLastCellNum();
+				List<String> rowList = new ArrayList<String>();
+				
 				for(int colIx = minCilIx;colIx < maxCilIx;colIx++){
 					HSSFCell cell = hssfRow.getCell(colIx);
 					if (cell == null) {
 						continue;
 					}
 					//将数据存入数组中
+					rowList.add(cell.getStringCellValue());
 				}
+				restult.add(rowList);
 			}
 		}
+		return restult;
+	}
+	
+	public void savStu() throws IOException{
+		List<List<String>> list = resExcel("demo_Student");
+		List<Student> students = new ArrayList<Student>();
+		for(int i = 0;i < list.size();i++){
+			Student student = new Student();
+			List<String> tempList = list.get(i);
+			student.setStudentId(tempList.get(0));
+			student.setStudentName(tempList.get(1));
+			student.setDepartmentId(Integer.valueOf(tempList.get(3)));
+			student.setClassId(Integer.valueOf(tempList.get(4)));
+			student.setPassword(tempList.get(5));
+			students.add(student);
+		}
+		Db.batchSave(students, list.size());
 	}
 }
