b8be10401594132260a9f1b400c5a4ba4e9a02c6
twei3131 <848270978@qq.com>
2016-09-20 09:40:12 +0800

    毕业实训

--- src/com/twei3131/service/ResolveExcel.java
+++ src/com/twei3131/service/ResolveExcel.java
@@ -1,12 +1,41 @@
 package com.twei3131.service;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 
 import com.jfinal.kit.PathKit;
 
 public class ResolveExcel {
 	public final String basePath = PathKit.getWebRootPath()+File.separator+"twei3131Load"+File.separator;
-	public void studentRes(){
-		
+	@SuppressWarnings("resource")
+	public void studentRes(String filename) throws IOException{
+		InputStream inStream = new FileInputStream(basePath+filename+".xlsx");
+		HSSFWorkbook hssfWorkbook = new HSSFWorkbook(inStream);
+		for(int numSheet = 0;numSheet < hssfWorkbook.getNumberOfSheets();numSheet++){
+			HSSFSheet hssfSheet = hssfWorkbook.getSheetAt(numSheet);
+			if (hssfSheet == null) {
+				continue;
+			}
+			for(int rowNum = 1;rowNum<hssfSheet.getLastRowNum();rowNum++){
+				HSSFRow hssfRow = hssfSheet.getRow(rowNum);
+				
+				int minCilIx = hssfRow.getFirstCellNum();
+				int maxCilIx = hssfRow.getLastCellNum();
+				for(int colIx = minCilIx;colIx < maxCilIx;colIx++){
+					HSSFCell cell = hssfRow.getCell(colIx);
+					if (cell == null) {
+						continue;
+					}
+					//将数据存入数组中
+				}
+			}
+		}
 	}
 }
